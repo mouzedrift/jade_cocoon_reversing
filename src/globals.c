c04fea4e8a9f0cb8ec56a54a0db2c6cdfa8fce5d
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2025-01-22 01:02:33 +0100

    match ReadFileIntoBuffer_8003CC54, clangformat

--- src/globals.c
+++ src/globals.c
@@ -4,6 +4,7 @@ int dword_8008BD84[93];
 int dword_800918B0;
 int dword_80096090[4];
 enum GameStates currentGameState_8008D4B4;
-int unknownFlags_8008D678;
+volatile int unknownFlags_8008D678;
 short word_8008B558[2];
 DslFILE Data001File_800954A8;
+int DsReadSyncResult_8008D630;
--- src/globals.h
+++ src/globals.h
@@ -1,15 +1,16 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-#include "enums.h"
 #include "LIBDS.H"
+#include "enums.h"
 
 extern int dword_8008BD84[93];
 extern int dword_800918B0;
 extern int dword_80096090[4];
 extern enum GameStates currentGameState_8008D4B4;
-extern int unknownFlags_8008D678;
+extern volatile int unknownFlags_8008D678;
 extern short word_8008B558[2];
 extern DslFILE Data001File_800954A8;
+extern int DsReadSyncResult_8008D630;
 
 #endif
